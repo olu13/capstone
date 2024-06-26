e58b8e117ef2b9ad4caf385785f1d8c8cfc99f3f
olu13 <69245639+olu13@users.noreply.github.com>
2024-06-26 06:53:40 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -56,23 +56,7 @@ resource "aws_security_group" "ecs" {
   }
 }
 
-resource "aws_iam_role" "ecs_task_execution_role" {
-  name               = "ecsTaskExecutionRole"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action    = "sts:AssumeRole",
-      Effect    = "Allow",
-      Principal = {
-        Service = "ecs-tasks.amazonaws.com"
-      }
-    }]
-  })
 
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
-  ]
-}
 
 resource "aws_ecs_cluster" "main" {
   name = "my-cluster"
