b40f241b7fd6c145ee0b6e23fcb5ef1bb814e704
olu13 <69245639+olu13@users.noreply.github.com>
2024-07-04 14:46:13 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,10 @@
 provider "aws" {
   region = "us-east-1"
 }
-
 resource "aws_ecs_cluster" "my_cluster" {
   name = "my-ecs-cluster"
 }
-
-// Task Definition
+// Use heredoc syntax to embed the JSON template directly
 resource "aws_ecs_task_definition" "my_task_definition" {
   family                = "my-task-family"
   container_definitions = <<-EOT
@@ -27,8 +25,6 @@ resource "aws_ecs_task_definition" "my_task_definition" {
     ]
   EOT
 }
-
-// ECS Service
 resource "aws_ecs_service" "my_service" {
   name            = "my-ecs-service"
   cluster         = aws_ecs_cluster.my_cluster.id
@@ -36,59 +32,38 @@ resource "aws_ecs_service" "my_service" {
   desired_count   = 1
   launch_type     = "EC2"
 }
-
-// Ensure instances are registered to the cluster
-resource "aws_instance" "capstone" {
-  ami             = "ami-06c68f701d8090592"
-  instance_type   = "t2.micro"
-  security_groups = ["sg-002f5802f23c919f3"]
-  subnet_id       = "subnet-0a52736d51a6edaf5"
-}
-
-// Load Balancer
 resource "aws_lb" "my_load_balancer" {
   name               = "my-load-balancer"
   internal           = false
   load_balancer_type = "application"
   security_groups    = ["sg-07a91622ffb6ac848"]
-  subnets            = [
-    "subnet-08b974908240191aa",
-    "subnet-0a52736d51a6edaf5",
-    "subnet-0981d8102bbdb520f",
-    "subnet-0f0bb9795e0215303",
-    "subnet-02c28d9ea478551a8",
-    "subnet-02e8d4b46a4f7d51f"
-  ]
+  subnets            = ["subnet-08b974908240191aa", "subnet-0a52736d51a6edaf5", "subnet-0981d8102bbdb520f", "subnet-0f0bb9795e0215303", "subnet-02c28d9ea478551a8", "subnet-02e8d4b46a4f7d51f"]
 }
-
-// Target Group
 resource "aws_lb_target_group" "my_target_group" {
   name     = "my-target-group"
   port     = 80
   protocol = "HTTP"
-  vpc_id   = "vpc-080ae127d0fdadb00"
+  vpc_id = "vpc-080ae127d0fdadb00"
 }
-
-// Listener
 resource "aws_lb_listener" "my_listener" {
   load_balancer_arn = aws_lb.my_load_balancer.arn
   port              = "80"
   protocol          = "HTTP"
-
   default_action {
     target_group_arn = aws_lb_target_group.my_target_group.arn
     type             = "forward"
   }
 }
-
-
-// Uncomment this section if you want to configure Route 53 record for your web app
-resource "aws_route53_record" "my-webapp-dns" {
-  depends_on = [aws_lb.my_load_balancer]  # Wait for the load balancer to be created
-  zone_id    = "Z0535352AWZD8KPMMKDN"
-  name       = "www.developerolu.com"
-  type       = "CNAME"  # Use CNAME instead of A
-  ttl        = "300"
-  records    = [aws_lb.my_load_balancer.dns_name] # Use the DNS name created by the LB
+resource "aws_lb_target_group_attachment" "ecs_attachments" {
+  target_group_arn = "arn:aws:elasticloadbalancing:us-east-1:471112982662:targetgroup/myTG/68ca8c0cde2befa2"
+  target_id        = "i-01d71f46920373e3e" // Replace with your EC2 instance ID
 }
 
+/*resource "aws_route53_record" "my-webapp-dns" {
+  depends_on = [aws_lb.my_load_balancer]  # Wait for the load balancer to be created
+  zone_id = "Z0535352AWZD8KPMMKDN"
+  name    = "www.developerolu.com"
+  type    = "CNAME"  # Use CNAME instead of A
+  ttl     = "300"
+  records = ["myALB-1695889425.us-east-1.elb.amazonaws.com"] # Use the DNS name created by the LB
+}*/
